db7a1942229e5c17862786a72b78a0f3feb044d1
Gerjan Stokkink <gerjan@bookingexperts.nl>
2022-09-14 14:15:04 +0200

    View full relative path in Lualine

--- nvim/lua/gstokkink/lualine.lua
+++ nvim/lua/gstokkink/lualine.lua
@@ -3,4 +3,12 @@ require('lualine').setup({
     icons_enabled = false,
     theme = 'onelight',
   },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
 })
