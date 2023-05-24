5bc776ee67fd7c7d58324b1ad9d7acc740366999
Gerjan Stokkink <gerjan@bookingexperts.nl>
2023-05-24 15:51:01 +0200

    Fix copilot-cmp exception

--- nvim/lua/gstokkink/plugins/cmp.lua
+++ nvim/lua/gstokkink/plugins/cmp.lua
@@ -26,7 +26,7 @@ return {
         -- fixes lazy-loading issues with the copilot cmp source
         require('gstokkink.utils').on_attach(function(client)
           if client.name == 'copilot' then
-            copilot_cmp._on_insert_enter()
+            copilot_cmp._on_insert_enter({})
           end
         end)
       end,
